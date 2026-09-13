60792f7717ae49a45ba9542fce5253b8abbf22c1
waldokolk-dotcom <waldokolk@gmail.com>
2026-09-13 22:01:36 +0200

    Bump app version to 1.6.3

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 (()=>{
-  const release={version:'1.6.2',name:'UI hotfix',date:'2026-09-13'};
+  const release={version:'1.6.3',name:'Stability hotfix',date:'2026-09-13'};
   window.WHATSUP_DOG_RELEASE=Object.freeze(release);
   function render(){
     document.documentElement.dataset.appVersion=release.version;
